43054affe2387b6243d4a68e55f42bde12dcf813
Kyle Balousek <kbalouse@umich.edu>
2015-12-11 00:10:10 -0500

    Facial recognition and alignment code

---
+++ alignCrop.m
@@ -0,0 +1,45 @@
+function outputImage = AlignCrop(imageName, model, Model3D, eyemask, REFTFORM, REFSZ)
+% Load query image
+I_Q = imread(imageName);
+
+fidu_XY = FacialFeatureDetection(I_Q, model);
+if isempty(fidu_XY)
+    %error('Failed to detect facial features / find face in image.');
+    fprintf('Failed to detect facial features on the image:');
+    fprintf(imageName);
+    fprintf(' Skipping image...\n');
+    outputImage = [];
+    return;
+end
+
+% Estimate projection matrix C_Q
+[C_Q, ~,~,~] = estimateCamera(Model3D, fidu_XY);
+
+% Render frontal view
+[frontal_sym, frontal_raw] = Frontalize(C_Q, I_Q, Model3D.refU, eyemask);
+
+
+% Apply similarity transform to LFW-a coordinate system, for compatability
+% with existing methods and results
+frontal_sym = imtransform(frontal_sym,REFTFORM,'XData',[1 REFSZ(2)], 'YData',[1 REFSZ(1)]);
+%frontal_raw = imtransform(frontal_raw,REFTFORM,'XData',[1 REFSZ(2)], 'YData',[1 REFSZ(1)]);
+
+fidu_XY = FacialFeatureDetection(frontal_sym, model);
+if isempty(fidu_XY)
+    %error('Failed to detect facial features / find face in image.');
+    fprintf('Failed to detect facial features on the image:');
+    fprintf(imageName);
+    fprintf(' Skipping image...\n');
+    outputImage = [];
+    return;
+end
+
+outputImage = imcrop(frontal_sym, [min(fidu_XY(:,1)) min(fidu_XY(:,2)) max(fidu_XY(:,1))-min(fidu_XY(:,1)) max(fidu_XY(:,2))-min(fidu_XY(:,2))]);
+
+% Display results
+%figure; imshow(I_Q); title('Query photo');
+%figure; imshow(I_Q); hold on; plot(fidu_XY(:,1),fidu_XY(:,2),'.'); hold off; title('Query photo with detections overlaid');
+%figure; imshow(frontal_raw); title('Frontalilzed no symmetry');
+%figure; imshow(frontal_sym); title('Frontalilzed with soft symmetry');
+%figure; imshow(imcrop(frontal_sym, [min(fidu_XY(:,1)) min(fidu_XY(:,2)) max(fidu_XY(:,1))-min(fidu_XY(:,1)) max(fidu_XY(:,2))-min(fidu_XY(:,2))]));
+end
